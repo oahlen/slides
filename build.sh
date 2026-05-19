82a12ffcf1a6e42aa17113bd9d5489399347d707
Oscar Ahlén <github@ahlen.dev>
2026-05-19 19:16:17 +0200

    Add landing page

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Cleanup
-rm -r out/
+rm -rf out/
 mkdir out/
 
-# Copy global assets
+# Copy global assets and landing page
 cp -r assets/ out/
+cp index.html out/index.html
 
 for dir in slides/*/; do
     name="${dir#slides/}"
@@ -13,4 +14,5 @@ for dir in slides/*/; do
     marp -I "$dir" --output "out/$name"
     cp -r "${dir}assets" "out/$name/assets" 2>/dev/null || true
     sed -Ei 's|([^.])/assets|\1../assets|g; s|^/assets|../assets|g' "out/$name/slides.html"
+    mv "out/$name/slides.html" "out/$name/index.html"
 done
